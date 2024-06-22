f36d0775f209bd9d9e827f250debab0441c30943
Akask-Rai-27 <spyeye118@gmail.com>
2024-06-23 00:32:42 +0530

    update

--- BlogApp/src/components/Header/Header.jsx
+++ BlogApp/src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
 
   const navigate = useNavigate()
 
-  const naItems = [
+  const navItems = [
     {
       name: 'Home',
       slug : '/',
@@ -44,8 +44,29 @@ function Header() {
     <header className='py-3 shadow bg-gray-500'>
     <Container>
 
-    <nav>
-      
+    <nav className='flex'>
+      <div className='mr-4'>
+        <Link to='/'>
+          <Logo width='70px'/>
+        </Link>
+      </div>
+      <ul className='flex ml-auto'>
+        {navItems.map((item)=>
+          item.active ? (
+            <li key={item.name}>
+              <button
+              onClick={()=> navigate(item.slug)}
+              className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+              >{item.name}</button>
+            </li>
+          ) : null
+        )}
+        {authStatus && (
+          <li>
+            <LogoutBtn/>
+          </li>
+        )}
+      </ul>
     </nav>
 
     </Container>
