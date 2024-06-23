e3301721254ad2e1ac11f98b2ba7c86e271f331c
Akask-Rai-27 <spyeye118@gmail.com>
2024-06-24 00:45:45 +0530

    learnt react-redux again

---
+++ React-redux/src/App.jsx
@@ -0,0 +1,17 @@
+import AddTodo from "./component/AddTodo"
+import Todos from "./component/Todos"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>Redux-toolkit</h2>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
